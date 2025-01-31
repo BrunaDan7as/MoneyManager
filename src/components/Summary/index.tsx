f952208322c007385d669dfabaa892d98f5505f9
BrunaDan7as <b.dantassoares@gmail.com>
2025-01-30 21:38:56 -0300

    Criando contexto de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,12 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useContext } from "react";
+
 import { SummaryCard, SummaryContainer } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContent";
+
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext)
+    console.log(transactions)
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -8,20 +14,25 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
+
                 <strong>R$ 17.400,00</strong>
             </SummaryCard>
+
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
+
                 <strong>R$ 17.400,00</strong>
             </SummaryCard>
+
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
+
                 <strong>R$ 17.400,00</strong>
             </SummaryCard>
         </SummaryContainer>
