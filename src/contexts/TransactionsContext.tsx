d93d7a9dc12e0d5eb4e6d33ce4596af47f83ec9d
BrunaDan7as <b.dantassoares@gmail.com>
2025-03-02 18:24:40 -0300

    Adicionando axios.

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+
 interface Transaction {
     id: number;
     description: string;
@@ -7,30 +9,36 @@ interface Transaction {
     category: string;
     createdAt: string;
 }
+
 interface TransactionContextType {
     transactions: Transaction[];
     fetchTransactions: (query?: string) => Promise<void>;
 }
+
 interface TransactionsProviderProps {
     children: ReactNode
 }
+
 export const TransactionsContext = createContext({} as TransactionContextType);
+
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     async function fetchTransactions(query?: string) {
-        const url = new URL('http://localhost:3333/transactions');
+        const response = await api.get('transactions', {
+            params: {
+                q: query,
+            }
+        })
 
-        if (query) {
-            url.searchParams.append('q', query);
-        }
 
-        const response = await fetch(url)
-        const data = await response.json()
-        setTransactions(data)
+        setTransactions(response.data)
     }
+
     useEffect(() => {
         fetchTransactions()
     }, []);
+
     return (
         <TransactionsContext.Provider value={{
             transactions,
